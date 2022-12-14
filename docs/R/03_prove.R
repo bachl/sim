19907baa724f955205e190657f4d969dc61674c8
Marko <marko.bachl@gmail.com>
2022-12-14 10:34:51 +0100

    Smaller updates; Exercise 2b

--- docs/R/03_prove.R
+++ docs/R/03_prove.R
@@ -1,4 +1,4 @@
-## ----setup, include=FALSE--------------------------------------------------------------------------
+## ----setup-----------------------------------------------------------------------------------------
 pacman::p_load(knitr, tidyverse)
 theme_set(hrbrthemes::theme_ipsum_rc(base_size = 25, axis_title_size = 25))
 
